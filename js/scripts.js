53214b30fe7fe195d084cefd7d743b8ecf09f0a5
yesthecarlos <carlosmendez86@gmail.com>
2021-03-26 15:25:24 -0700

    implement basic branching

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,32 @@ $(document).ready(function () {
     const iosInput = $("input:radio[name=ios]:checked").val();
     const startInput = $("#start").val();
     const puzzleInput = $("input:radio[name=puzzle]:checked").val();
-  $(".username").append(usernameInput);
+  $(".username").empty().append(usernameInput);
   console.log(zodiacInput);
   console.log(proofreaderInput);
   console.log(iosInput);
   console.log(startInput);
   console.log(puzzleInput);
     let result;
-    if (zodiacInput === "aries") {
+    if (iosInput === "true") {
       result = "Swift";
-    }
-    $(".result").append(result);
+    } else if (proofreaderInput === "false" && puzzleInput === "true") {
+      result = "JavaScript"
+    } else if (proofreaderInput === "false" && puzzleInput === "false") {
+    result = "Python";
+    } else if (proofreaderInput === "true" && puzzleInput === "false") {
+      result = "Ruby";
+    } else if (proofreaderInput === "true" && puzzleInput === "true") {
+      result = "C#";
+      }
+    
+    // else if (startInput > Date(2021, 2, 26) {
+    //   result = "Python"
+    
+    $(".result").empty().append(result);
 
     $("#output").show();
-  event.preventDefault();
+    event.preventDefault();
+  
   });
 });
-
