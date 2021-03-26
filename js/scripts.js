a00fd8f75b0531e11fd138624f3549cb366f0536
yesthecarlos <carlosmendez86@gmail.com>
2021-03-26 13:48:33 -0700

    add proofreader, ios, start date, and puzzle questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
-$("form#formOne").submit(function(event) {
-  const zodiac = $("input:radio[name=zodiac]:checked").val();
-  const usernameInput = $("input#username").val();
-console.log(zodiac);
-$(".username").append(usernameInput);
-
-$("#output").show();
+  $("form#formOne").submit(function(event) {
+    const usernameInput = $("input#username").val();
+    const zodiacInput = $("input:radio[name=zodiac]:checked").val();
+    const proofreaderInput = $("input:radio[name=proofreader]:checked").val();
+    const iosInput = $("input:radio[name=ios]:checked").val();
+    const startInput = $("#start").val();
+    const puzzleInput = $("input:radio[name=puzzle]:checked").val();
+  $(".username").append(usernameInput);
+  console.log(zodiacInput);
+  console.log(proofreaderInput);
+  console.log(iosInput);
+  console.log(startInput);
+  console.log(puzzleInput);
+  $("#output").show();
   event.preventDefault();
-});
+  });
 });
