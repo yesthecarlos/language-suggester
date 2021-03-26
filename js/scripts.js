3e61c3f941ec9b343f2fa21c3d569345ec4eeac0
yesthecarlos <carlosmendez86@gmail.com>
2021-03-26 10:55:42 -0700

    add return of user's name to results

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
 $("form#formOne").submit(function(event) {
   const zodiac = $("input:radio[name=zodiac]:checked").val();
+  const usernameInput = $("input#username").val();
 console.log(zodiac);
+$(".username").append(usernameInput);
 
+$("#output").show();
   event.preventDefault();
 });
 });
